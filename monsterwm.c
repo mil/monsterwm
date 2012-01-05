7c7b90f75aaecb7d79c7fcc434a42da05b694bdc
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-01-05 14:18:55 +0200

    monocle layout and single windows with borders

--- monsterwm.c
+++ monsterwm.c
@@ -458,11 +458,7 @@ void enternotify(XEvent *e) {
  * highlight borders and set active window property and
  * give input focus to the current/active/focused client
  *
- * a window should have borders in any case, except if
- *  - the window is fullscreen
- *  - the window is not floating or transient and
- *      - the mode is MONOCLE or,
- *      - it is the only window on screen
+ * windows always get borders, except if fullscreen
  */
 void focus(Client *c, Desktop *d) {
     /* update references to prev and curr,
@@ -534,8 +530,7 @@ void focus(Client *c, Desktop *d) {
     w[(d->curr->isfloat || d->curr->istrans) ? 0:ft] = d->curr->win;
     for (fl += !ISFFT(d->curr) ? 1:0, c = d->head; c; c = c->next) {
         XSetWindowBorder(dis, c->win, c == d->curr ? win_focus:win_unfocus);
-        XSetWindowBorderWidth(dis, c->win, c->isfull || (!ISFFT(c) &&
-            (d->mode == MONOCLE || !d->head->next)) ? 0:BORDER_WIDTH);
+        XSetWindowBorderWidth(dis, c->win, c->isfull ? 0:BORDER_WIDTH);
         if (c != d->curr) w[c->isfull ? --fl:ISFFT(c) ? --ft:--n] = c->win;
         if (CLICK_TO_FOCUS || c == d->curr) grabbuttons(c);
     }
@@ -781,7 +776,8 @@ void mousemotion(const Arg *arg) {
  * each window should cover all the available screen space
  */
 void monocle(int hh, int cy, Desktop *d) {
-    for (Client *c = d->head; c; c = c->next) if (!ISFFT(c)) XMVRSZ(dis, c->win, 0, cy, ww, hh);
+    for (Client *c = d->head; c; c = c->next) if (!ISFFT(c))
+        XMVRSZ(dis, c->win, 0, cy, ww - 2*BORDER_WIDTH, hh - 2*BORDER_WIDTH);
 }
 
 /**
